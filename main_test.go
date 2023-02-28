0bbbec172639d9cfcf59e5029fa98989069921ea
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-28 18:15:19 -0500

    test(root): Add advanced tests

--- main_test.go
+++ main_test.go
@@ -1,3 +1,40 @@
+package main
+
+import "testing"
+
+var tests = []struct {
+	name     string
+	dividend float32
+	divisor  float32
+	expected float32
+	isError  bool
+}{
+	{"valid-data", 100.0, 10.0, 10.0, false},
+	{"invalid-data", 100.0, 0.0, 0.0, true},
+	{"exptect-5", 50.0, 10.0, 5.0, false},
+	{"exptect-fraction", -1.0, -777.0, 0.0012870013, false},
+}
+
+func TestDivision(t *testing.T) {
+	for _, tt := range tests {
+		got, err := divide(tt.dividend, tt.divisor)
+
+		if tt.isError {
+			if err == nil {
+				t.Error("Expected and error but did not get one")
+			}
+		} else {
+			if err != nil {
+				t.Error("Did not expect and error but gone one", err.Error())
+			}
+		}
+
+		if got != tt.expected {
+			t.Errorf("Expected %f but got %f", tt.expected, got)
+		}
+	}
+}
+
 // func TestDivide(t *testing.T) {
 // 	_, err := divide(10.0, 1.0)
 // 	if err != nil {
