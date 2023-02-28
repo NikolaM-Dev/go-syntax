c6e25dfc17301750f6d54abf6bdeeae4be86f2c3
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-28 18:13:23 -0500

    feat(root): Add conditionals

--- main.go
+++ main.go
@@ -112,3 +112,14 @@ import (
 //
 // 	log.Println(names)
 // }
+
+// NOTE: conditionals
+// func main() {
+// 	cat := "czat"
+//
+// 	if cat == "cat" {
+// 		log.Println("Cat is cat")
+// 	} else {
+// 		log.Println("Cat is not cat")
+// 	}
+// }
