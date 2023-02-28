5ffe279ee64009423270bb0cd07f1afd231ab3a7
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-28 18:15:08 -0500

    test(root): Add basic tests

---
+++ main_test.go
@@ -0,0 +1,13 @@
+// func TestDivide(t *testing.T) {
+// 	_, err := divide(10.0, 1.0)
+// 	if err != nil {
+// 		t.Error("Got and error we should not have")
+// 	}
+// }
+//
+// func TestBadDivide(t *testing.T) {
+// 	_, err := divide(10.0, 0)
+// 	if err == nil {
+// 		t.Error("Did not get an error when we should have")
+// 	}
+// }
