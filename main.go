935d57a9211a2be269f7f52ccf64c7028e5b37e8
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-28 18:11:43 -0500

    feat(root): Add basics

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"log"
+)
+
+// NOTE: Basics
+// func main() {
+// 	fmt.Println("Hello World")
+//
+// 	var whatToSay string
+// 	var i int
+//
+// 	whatToSay = "Goodby, cruel world"
+//
+// 	fmt.Println(whatToSay)
+//
+// 	i = 7
+//
+// 	fmt.Println("i is set to", i)
+//
+// 	whatWasSaid, theOtherThingThatSaid := saySomething()
+//
+// 	fmt.Println("The function returned", whatWasSaid, theOtherThingThatSaid)
+// }
+//
+// func saySomething() (string, string) {
+// 	return "something", "else"
+// }
