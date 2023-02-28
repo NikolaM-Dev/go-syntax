96487b3e9f6f0520c872aabcda9c0afaef59d6aa
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-28 18:12:05 -0500

    feat(root): Add pointers

--- main.go
+++ main.go
@@ -28,3 +28,20 @@ import (
 // func saySomething() (string, string) {
 // 	return "something", "else"
 // }
+
+// NOTE: Pointers
+// func main() {
+// 	myString := "Green"
+//
+// 	log.Println("myString is set to", myString)
+//
+// 	changeUsingPointer(&myString)
+//
+// 	log.Println("after func call myString is set to", myString)
+// }
+//
+// func changeUsingPointer(s *string) {
+// 	log.Println("s is set to", *s)
+// 	newValue := "Red"
+// 	*s = newValue
+// }
