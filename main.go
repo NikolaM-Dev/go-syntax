403e1fd5ede7e2183d730f110f6f165f75bb1702
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-28 18:12:33 -0500

    feat(root): Add methods

--- main.go
+++ main.go
@@ -65,3 +65,24 @@ import (
 //
 // 	log.Println(user.FirstName, user.LastName, "BirthDate", user.BirthDate)
 // }
+
+// NOTE: methods
+// type myStruct struct {
+// 	FirstName string
+// }
+//
+// func (m *myStruct) printFirstName() string {
+// 	return m.FirstName
+// }
+//
+// func main() {
+// 	var myVar myStruct
+// 	myVar.FirstName = "John"
+//
+// 	myVar2 := myStruct{
+// 		FirstName: "Mary",
+// 	}
+//
+// 	log.Println("myVar is set to", myVar.printFirstName())
+// 	log.Println("myVar2 is set to", myVar2.printFirstName())
+// }
