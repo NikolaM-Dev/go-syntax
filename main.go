0ee88cd6d4aecb834959d34a2b1db76bfe6d941f
Juan David Merchan Torres <juan1014166@gmail.com>
2023-02-28 18:14:24 -0500

    feat(root): Add testing

--- main.go
+++ main.go
@@ -217,3 +217,27 @@ import (
 //
 // 	fmt.Println("newJSon:", string(newJson))
 // }
+
+// NOTE: Testing
+func main() {
+	result, err := divide(100.0, 0.0)
+	if err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	log.Println("Result of division is", result)
+}
+
+func divide(x, y float32) (float32, error) {
+	var result float32
+
+	if y == 0 {
+		return result, errors.New("cannot divide by 0")
+	}
+
+	result = x / y
+
+	return result, nil
+}
